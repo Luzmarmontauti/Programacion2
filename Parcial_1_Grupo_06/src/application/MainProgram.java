dbd6e7218cdadc2e642ddc6f901f11ab7b0c9d63
luzma <luzma@10.100.10.69>
2026-04-10 19:32:56 -0300

    Agregando estructuras al main program

--- Parcial_1_Grupo_06/src/application/MainProgram.java
+++ Parcial_1_Grupo_06/src/application/MainProgram.java
@@ -2,14 +2,31 @@ package application;
 import java.util.Scanner;
 
 public class MainProgram {
+	
+	private boolean running = true;
+	
+	private Exercise exercise;
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in);
+		MainProgram program = new MainProgram();
 		
+		program.run();
 		
+
 
 	}
-
+	
+	private void run() {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (running) {
+			
+		}
+	}
 }
